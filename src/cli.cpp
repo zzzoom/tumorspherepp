5378ab3df4ab8f55bc8a8760ff5adb15e2c18650
Carlos Bederian <carlos.bederian@unc.edu.ar>
2024-07-12 14:58:55 -0300

    Parallelize runs with OpenMP

--- src/cli.cpp
+++ src/cli.cpp
@@ -8,9 +8,10 @@
 #include <iostream>
 #include <string>
 
-static void run(float prob_stem, float prob_diff, int steps, std::string name)
+static void run(float prob_stem, float prob_diff, int realization, int steps)
 {
-    Output out(name);
+    std::string run_name = std::format("ps{}_pd{}_r{}", prob_stem, prob_diff, realization);
+    Output out(run_name);
     Culture culture(1.0f, &out);
     culture.addCell(Position { 0.0f, 0.0f, 0.0f }, true, 0, 0);
     culture.prob_diff = prob_stem;
@@ -22,7 +23,15 @@ int main(int argc, char** argv)
 {
     cxxopts::Options options("tumorsphere", "Simulates tumorsphere growth with the indicated parameters.");
 
-    options.add_options()("prob-stem", "List of probabilities of stem cells", cxxopts::value<std::vector<float>>())("prob-diff", "List of probabilities of differentiated cells", cxxopts::value<std::vector<float>>())("realizations", "Number of realizations", cxxopts::value<int>())("steps-per-realization", "Number of steps per realization", cxxopts::value<int>())("rng-seed", "Random number generator seed", cxxopts::value<int>())("h,help", "Print usage");
+    // clang-format off
+    options.add_options()
+        ("prob-stem", "List of probabilities of stem cells", cxxopts::value<std::vector<float>>())
+        ("prob-diff", "List of probabilities of differentiated cells", cxxopts::value<std::vector<float>>())
+        ("realizations", "Number of realizations", cxxopts::value<int>())
+        ("steps-per-realization", "Number of steps per realization", cxxopts::value<int>())
+        ("rng-seed", "Random number generator seed", cxxopts::value<int>())
+        ("h,help", "Print usage");
+    // clang-format on
 
     auto options_result = options.parse(argc, argv);
 
@@ -36,11 +45,13 @@ int main(int argc, char** argv)
     auto realizations = options_result["realizations"].as<int>();
     auto steps_per_realization = options_result["steps-per-realization"].as<int>();
 
+#pragma omp parallel
+#pragma omp single
     for (const auto ps : pstems) {
         for (const auto pd : pdiffs) {
             for (int r = 0; r < realizations; ++r) {
-                std::string run_name = std::format("ps{}_pd{}_r{}", ps, pd, r);
-                run(ps, pd, steps_per_realization, run_name);
+#pragma omp task
+                run(ps, pd, r, steps_per_realization);
             }
         }
     }
